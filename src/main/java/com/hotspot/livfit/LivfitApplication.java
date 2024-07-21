b9127812b9c4549a50ca2d51c3685ee52e895f99
GENIE <hyunjin117@naver.com>
2024-07-21 16:56:46 +0900

    ♻️Refactor: Spotless 적용

--- src/main/java/com/hotspot/livfit/LivfitApplication.java
+++ src/main/java/com/hotspot/livfit/LivfitApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LivfitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LivfitApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(LivfitApplication.class, args);
+  }
 }
--- src/main/java/com/hotspot/livfit/config/swagger/SwaggerConfig.java
+++ src/main/java/com/hotspot/livfit/config/swagger/SwaggerConfig.java
@@ -1,11 +1,10 @@
 package com.hotspot.livfit.config.swagger;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springdoc.core.models.GroupedOpenApi;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 
 // 스웨거 접속 주소
@@ -14,18 +13,14 @@ import io.swagger.v3.oas.models.info.Info;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(
-                        new Info()
-                                .title("livfit API")
-                                .version("1.0")
-                                .description("livfit API description"));
-    }
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new Info().title("livfit API").version("1.0").description("livfit API description"));
+  }
 
-    @Bean
-    public GroupedOpenApi customGroupedOpenApi() {
-        return GroupedOpenApi.builder().group("api").pathsToMatch("/**").build();
-    }
+  @Bean
+  public GroupedOpenApi customGroupedOpenApi() {
+    return GroupedOpenApi.builder().group("api").pathsToMatch("/**").build();
+  }
 }
