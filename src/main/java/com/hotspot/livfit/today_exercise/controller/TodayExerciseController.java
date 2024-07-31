d0c6cb08a200f92a4b2168850780aafb58b5523e
april0114 <sue5520607@skuniv.ac.kr>
2024-08-01 04:22:09 +0900

    ♻️ Refactor: 오늘의 운동
    - 스웨거 설명 추가
    - 설명 주석 추가

--- src/main/java/com/hotspot/livfit/today_exercise/controller/TodayExerciseController.java
+++ src/main/java/com/hotspot/livfit/today_exercise/controller/TodayExerciseController.java
@@ -18,7 +18,9 @@ import com.hotspot.livfit.today_exercise.entity.TodayExercise;
 import com.hotspot.livfit.today_exercise.entity.TodayExerciseUser;
 import com.hotspot.livfit.today_exercise.service.TodayExerciseService;
 import com.hotspot.livfit.user.util.JwtUtil;
+
 import io.jsonwebtoken.Claims;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/api/today_exercise")
@@ -29,22 +31,26 @@ public class TodayExerciseController {
   private final JwtUtil jwtUtil;
   private static final Logger logger = LoggerFactory.getLogger(TodayExerciseController.class);
 
+  // 오늘의 운동 기록 저장
   /*
-   * URL: api/today_exercise/show/
-   * HTTP Method: GET
-   * 토큰 필요 x
+   * URL: /api/today_exercise/save
+   * HTTP Method: POST
+   * HTTP Body: record (JSON 형식)
+   * 요청 JSON 형식:
+   * {
+   *   "login_id": "test_dev", // users의 login_id 참조
+   *   "success": "success",
+   *   "dayOfWeek": "MONDAY",
+   * }
    */
 
   // 오늘의 운동 기록 저장하기
+  @Operation(summary = "오늘의 운동 기록 저장", description = "사용자의 오늘의 운동 기록 저장하기")
   @PostMapping("/save")
   public ResponseEntity<?> saveTodayExercise(
       @RequestHeader("Authorization") String bearerToken,
       @RequestBody TodayExerciseDTO todayExerciseRequest) {
 
-    if (bearerToken == null || !bearerToken.startsWith("Bearer ")) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-          .body("Invalid Authorization header format.");
-    }
     String token = bearerToken.substring(7).trim();
     if (token.isEmpty() || token.split("\\.").length != 3) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid JWT token format.");
@@ -53,16 +59,18 @@ public class TodayExerciseController {
     try {
       Claims claims = jwtUtil.getAllClaimsFromToken(token);
       String jwtLoginId = claims.getId(); // JWT에서 사용자 ID를 추출
-      DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
+      DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek(); // 오늘 날짜에서 요일 뽑아오기
 
       // TodayExerciseService를 통해 오늘의 운동 기록을 저장
       TodayExerciseUser todayExerciseUser =
           todayExerciseService.saveChallenge(
               jwtLoginId, dayOfWeek, todayExerciseRequest.getSuccess());
       System.out.println(dayOfWeek);
+      // 로그
       logger.info("일일 운동 기록 저장됨: {}", todayExerciseUser);
 
-      return ResponseEntity.ok(todayExerciseUser);
+      // 기록 저장 성공시 띄울 메시지
+      return ResponseEntity.ok().body("오늘의 운동 기록 저장 완료");
 
     } catch (Exception e) {
       log.error("An error occurred while saving the today exercise record: {}", e.getMessage());
@@ -71,7 +79,14 @@ public class TodayExerciseController {
     }
   }
 
+  /*
+   * URL: api/today_exercise/show/
+   * HTTP Method: GET
+   * 토큰 필요 x
+   */
+
   // 번호에 맞춰서 프론트 엔드가 사용하게[오늘의 운동 리스트 아이디]
+  @Operation(summary = "db에 있는 오늘의 운동 조회", description = "db에 있는 오늘의 운동 종류 가져오기")
   @GetMapping("/show/{id}")
   public ResponseEntity<TodayExercise> getTodayExerciseById(@PathVariable Long id) {
     Optional<TodayExercise> todayExercise = todayExerciseService.getTodayExercises(id);
