c34fd8743d5366e7f25b33d1b4c12d140b22e851
april0114 <sue5520607@skuniv.ac.kr>
2024-07-31 20:29:46 +0900

    🐛 Fix : 거북이 날짜 추가 & 기록 조회 가능
    - USER를 받아와 조회 가능

--- src/main/java/com/hotspot/livfit/turtle/dto/TurtleDTO.java
+++ src/main/java/com/hotspot/livfit/turtle/dto/TurtleDTO.java
@@ -1,13 +1,22 @@
 package com.hotspot.livfit.turtle.dto;
 
-import lombok.AllArgsConstructor;
+import java.time.LocalDate;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class TurtleDTO {
   private String nickname;
   private int score;
+  private LocalDate localDate;
+
+  public TurtleDTO() {}
+
+  public TurtleDTO(String nickname, int score, LocalDate localDate) {
+    this.nickname = nickname;
+    this.score = score;
+    this.localDate = localDate;
+  }
 }
